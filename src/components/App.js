037457279375c8f4e03f987cc6e5860d3dee86e5
wathika-eng <wathika02@gmail.com>
2024-05-11 10:49:59 +0300

    loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+	const [image, setImage] = useState();
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setLoading(true);
+		fetch('https://dog.ceo/api/breeds/image/random')
+			.then((res) => res.json())
+			.then((res) => {
+				setImage(res.message);
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.error('Error');
+				setLoading(false);
+			});
+	}, []);
+	return (
+		<div>
+			{image ? <img src={image} alt='A Random Dog' /> : <p>Loading...</p>}
+		</div>
+	);
+};
+
+export default App;
